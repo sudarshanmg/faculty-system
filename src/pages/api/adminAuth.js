d59f921eb34d129747b46887cc7ab4edd67c6ca3
sudarshan <sudarshanmallibhat@gmail.com>
2023-02-27 12:55:03 +0530

    api error 2

--- src/pages/api/adminAuth.js
+++ src/pages/api/adminAuth.js
@@ -2,8 +2,18 @@ import supabaseAdmin from '@/lib/supabaseAdmin';
 
 export default async function handler(req, res) {
   const password = req.body.password;
-  console.log(password);
-  res.json({auth: 'ok'});
+  console.log('pass', password);
+
+  try {
+    const { data, error } = await supabaseAdmin.from('admin').select('passw');
+    if (data) {
+      res.json({auth: 'ok'});
+    }
+  } catch (error) {
+    console.log(error);
+  } finally {
+    res.send('admin');
+  }
 
   //   const hashedPassword = rows[0].password;
 
