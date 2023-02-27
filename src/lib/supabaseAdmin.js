ef3c6de58acc8a8994e80f5884de17fa71dc1051
sudarshan <sudarshanmallibhat@gmail.com>
2023-02-27 11:39:04 +0530

    ...

--- src/lib/supabaseAdmin.js
+++ src/lib/supabaseAdmin.js
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabaseAdmin = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_SERVICE_KEY
+  process.env.SUPABASE_SERVICE_KEY
 );
 
 export default supabaseAdmin;
